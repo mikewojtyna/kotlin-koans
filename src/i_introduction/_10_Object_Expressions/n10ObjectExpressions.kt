d22e9ba309f9e283deb5d4fda637edeb140d195d
Mike Wojtyna <mike@buildmysoftware.pro>
2018-05-26 22:47:57 +0200

    Change solution of object expressions

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -3,7 +3,7 @@ package i_introduction._10_Object_Expressions
 import util.TODO
 import util.doc10
 import java.util.*
-import kotlin.Comparator
+import java.util.Collections.sort
 
 fun todoTask10(): Nothing = TODO(
     """
@@ -19,11 +19,8 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object : Comparator<Int> {
-        override fun compare(o1: Int?, o2: Int?): Int {
-            return o2!!.compareTo(o1!!)
-        }
-
+    sort(arrayList, object : Comparator<Int> {
+        override fun compare(o1: Int, o2: Int) = o2.compareTo(o1)
     })
     return arrayList
 }
