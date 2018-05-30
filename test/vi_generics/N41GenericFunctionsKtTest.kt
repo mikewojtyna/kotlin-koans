e94e4da546c3b69a6c75892e25dca95535507914
Mike Wojtyna <mike@buildmysoftware.pro>
2018-05-30 15:11:33 +0200

    Solve vi_generics koans

--- test/vi_generics/N41GenericFunctionsKtTest.kt
+++ test/vi_generics/N41GenericFunctionsKtTest.kt
@@ -4,14 +4,26 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class N41GenericFunctionsKtTest {
-    @Test fun testPartitionWordsAndLines() {
-        val (words, lines) = listOf("a", "a b", "c", "d e").partitionWordsAndLines()
+    @Test
+    fun testPartitionWordsAndLines() {
+        val (words, lines) = listOf(
+            "a",
+            "a b",
+            "c",
+            "d e"
+        ).partitionWordsAndLines()
         assertEquals(listOf("a", "c"), words)
         assertEquals(listOf("a b", "d e"), lines)
     }
 
-    @Test fun testPartitionLettersAndOtherSymbols() {
-        val (letters, other) = setOf('a', '%', 'r', '}').partitionLettersAndOtherSymbols()
+    @Test
+    fun testPartitionLettersAndOtherSymbols() {
+        val (letters, other) = setOf(
+            'a',
+            '%',
+            'r',
+            '}'
+        ).partitionLettersAndOtherSymbols()
         assertEquals(setOf('a', 'r'), letters)
         assertEquals(setOf('%', '}'), other)
     }
